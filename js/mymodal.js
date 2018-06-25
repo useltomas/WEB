bc9eecb7c9de452e03c4d390cc08d3d2e88544fb
Vytautas <vytautas.valiukonis@gmail.com>
2018-06-25 20:48:49 +0300

    komentaras3

---
+++ js/mymodal.js
@@ -0,0 +1,11 @@
+$(".box img").click(function(){
+  var mazoPaveiksliukoSrc = $(this).prop("src");
+  var naujasPaveiksliukas = $("<img>");
+  naujasPaveiksliukas.prop("src",mazoPaveiksliukoSrc);
+  $(".mymodalbox").append(naujasPaveiksliukas);
+  $(".mymodal").css({"display":"flex"});
+})
+
+$(".mymodal").click(function(){
+  $(".mymodal").css({"display":"none"});
+})
